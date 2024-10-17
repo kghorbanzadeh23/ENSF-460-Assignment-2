6c1f293cf58f86a7b6732ef7ca6069e566b8e351
HuttonLedingham <huttonledingham@gmail.com>
2024-10-17 15:58:32 -0600

    comments added

--- project1.X/IOs.c
+++ project1.X/IOs.c
@@ -22,24 +22,23 @@
 typedef enum{
     NOTHING_PRESSED,    //When no buttons are pressed
     BUTTON_PRESSED,     //When a button is pushed or let go          
-    TIMER_CHANGE,
-    TIMER_COUNTDOWN,              
-    TIMER_PAUSED,
-    TIMER_IDLE,
-    TIMER_COMPLETED
+    TIMER_CHANGE,       //When changing the time with PB1 and PB2
+    TIMER_COUNTDOWN,    //To update the timer when timer3 interrupts
+    TIMER_PAUSED,       //For when the timer is paused
+    TIMER_IDLE,         //When the program is waiting for an interrupt
+    TIMER_COMPLETED     //When the timer reaches to 0
 } states;
 
 states state = NOTHING_PRESSED; //Keeps track of state
 uint16_t Blinking_Interval = 0; //Tracks the Blinking_interval
-uint8_t seconds = 0;
-uint8_t minutes = 0;
-int8_t deltaSec = 0;
-int8_t deltaMin = 0;
+uint8_t seconds = 0;    //Keep track of seconds
+uint8_t minutes = 0;    //Keep track of minutes
+int8_t deltaSec = 0;    //To keep track of what to change the seconds by according to the buttons
+int8_t deltaMin = 0;    //To keep track of what to change the minutes by according to the buttons
 uint8_t CNflag; //Tracks if there was a CN interrupt
-uint8_t timerPaused = 1;
-uint8_t timer3Flag = 0;
-uint8_t timerActive = 0;
-uint8_t PB2Counter = 0;
+uint8_t timerPaused = 0;    //Keeps rtrack if timer is paused
+uint8_t timerActive = 0;    //Keeps tracks if the timer is active 
+uint8_t PB2Counter = 0;     //For the functionality of incrementing the seconds by 5
 
 void IOinit(){
     
@@ -47,18 +46,6 @@ void IOinit(){
     
     newClk(500);    //Switch clock to 500khz
     
-//    //T3CON config
-//    T2CONbits.T32 = 0; // operate timer 2 as 16 bit timer
-//    T3CONbits.TCKPS = 1; // set prescaler to 1:8
-//    T3CONbits.TCS = 0; // use internal clock
-//    T3CONbits.TSIDL = 0; //operate in idle mode
-//    IPC2bits.T3IP = 2; //7 is highest and 1 is lowest pri.
-//    IFS0bits.T3IF = 0;
-//    IEC0bits.T3IE = 1; //enable timer interrupt
-//    PR3 = 15625/5; // set the count value for 0.5 s (or 500 ms)
-//    TMR3 = 0;
-//    T3CONbits.TON = 0;
-
     /* Let's set up some I/O */
     TRISBbits.TRISB8 = 0;   //Set as output
     
@@ -89,64 +76,64 @@ void IOinit(){
 
 void IOcheck(){
     switch(state){
-        case NOTHING_PRESSED:        //State for when no buttons are pressed
-            LEDOUT = 0;
+        case NOTHING_PRESSED:        //State for when no buttons are pressed and timer is not active only when setting the timer
+            LEDOUT = 0;             
             Idle();                 //Idle until next interrupt either the timer or a new button pressed
             break;
             
         case BUTTON_PRESSED:        //When any of the buttons states changed
-            if(!PB3 && PB2 && PB1){
-                T3CONbits.TON = 0;
-                CNflag = 0;
+            if(!PB3 && PB2 && PB1){ //Checks if only PB3 was pressed
+                T3CONbits.TON = 0;  //Disable the countdown
+                CNflag = 0;         //Set up for checking for PB3 > 3 seconds
 
-                delay_ms(3000);
+                delay_ms(3000);     //delay_ms goes into idle so either the timer interrupt or button
                 T2CONbits.TON = 0;
                 
-                if(CNflag && (seconds || minutes)){
-                    if(!timerActive){
-                        startTimer();
-                        timerActive = 1;
-                        sendMessage("CNT ");
+                if(CNflag && (seconds || minutes)){ //Checks if button caused interrupt
+                    if(!timerActive){   //If the timer hasn't been started yet
+                        startTimer();   //Starts the timer
+                        timerActive = 1;    //Sets flag
+                        sendMessage("CNT ");    //Send countdown message
 
                         break;
                     }
                     
-                    if(timerPaused){
-                       state = TIMER_IDLE;
-                       T3CONbits.TON = 1;
+                    if(timerPaused){    //If timer has been started check if paused
+                       state = TIMER_IDLE;  //Sets to timer_idle if paused
+                       T3CONbits.TON = 1;   //Re-enable the timer
 
-                       timerPaused = 0;
+                       timerPaused = 0;     //Disable the paused flag
                     }
-                    else if(!timerPaused){
-                        timerPaused = 1;
-                        state = TIMER_PAUSED;
+                    else if(!timerPaused){  //If timer is not paused
+                        timerPaused = 1;    //Set flag to paused
+                        state = TIMER_PAUSED;   //State to TIMER_PAUSED
                     }
 
                 }
-                else if(!CNflag){
-                    seconds = 0;
-                    minutes = 0;
-                    state = NOTHING_PRESSED;
-                    timerPaused = 1;
+                else if(!CNflag){   //If the timer caused the interrupt
+                    seconds = 0;    //Reset the seconds
+                    minutes = 0;    //Reset the minues
+                    state = NOTHING_PRESSED;    //Go back to default state
+                    timerPaused = 0;            //Reset to dafault states
                     timerActive = 0;
-                    TMR3 = 0;
+                    TMR3 = 0;                   //Reset TMR3 counter
                     
-                    sendMessage("CLR ");
+                    sendMessage("CLR ");        //Send clear message
                 }
                 else{
-                    state = NOTHING_PRESSED;
+                    state = NOTHING_PRESSED;    //If seconds and minutes are not set go back to nothing pressed
 
                 }
                 break;
             }
             
             
-            if(timerActive){
+            if(timerActive){    //If the timer is active ignore other inputs
                 break;
             }
             
             CNflag = 0;             //Reset CNflag
-            delay_ms(50);
+            delay_ms(50);           //To prevent bounce on the buttons
             
             if(CNflag){
                 break;  //If debounce occurs or new button is pressed leave this case and read the button states again
@@ -155,88 +142,106 @@ void IOcheck(){
             
             //Which push buttons are pressed and which combination is pressed
             if (!PB2 && !PB1) { //If Push button 1 and 2 are pressed
-                deltaMin = 1;
-                deltaSec = 1;
-                state = TIMER_CHANGE;             //Switch state to blinking as only one button is pressed
+                deltaMin = 1;   //Changing the seconds by 1
+                deltaSec = 1;   //Changing the minutes by 1
+                state = TIMER_CHANGE;   //Switch state to TIMER_CHANGE to update the minutes and seconds
             }
             else if (!PB1){ //If only push button 1 is pressed
-                deltaSec = 0;
-                deltaMin = 1;
-                state = TIMER_CHANGE; 
-                PB2Counter = 0;
+                deltaSec = 0;   //Changing the seconds by 0
+                deltaMin = 1;   //Changing the minutes by 1
+                state = TIMER_CHANGE;   //Switch state to TIMER_CHANGE to update the minutes and seconds           
+                PB2Counter = 0; //Reset PB2 coutner as PB2 is not pressed
             }
             else if(!PB2){ //If only push button 2 is pressed
-                deltaSec = 1;
-                deltaMin = 0;
-                state = TIMER_CHANGE;             //Switch state to blinking as only one button is pressed
+                deltaSec = 1;   //Changing the seconds by 1
+                deltaMin = 0;   //Changing the minutes by 0 
+                state = TIMER_CHANGE;   //Switch state to TIMER_CHANGE to update the minutes and seconds
             }
-            else{
-                PB2Counter = 0;
-                state = NOTHING_PRESSED;
+            else{   //If no buttons are pressed
+                PB2Counter = 0; //Reset the PB2 counter as PB2 is not pressed
+                state = NOTHING_PRESSED;    //Go back to NOTHING_PRESSED
             }
 
 
             break;
             
-        case TIMER_CHANGE:  
-            changeTime(deltaSec, deltaMin);
-            sendMessage("SET ");
-            delay_ms(200);    //Pause the code here until an interrupt 
+        case TIMER_CHANGE: //Handle the changing of the timer 
+            changeTime(deltaSec, deltaMin); //Handles changing the seconds and minutes with values defined in BUTTON_PRESSED
+            sendMessage("SET ");    //Send updated time
+            delay_ms(200);    //Pause the code here so the program is more controllable by the user
             
-            if(PB2Counter >= 10){
-                deltaSec = 5;
+            if(PB2Counter >= 10){   //Checks if PB2 is held for 2 seconds
+                deltaSec = 5;   //Start adding 5 seconds per loop
             }
             else{
-                PB2Counter++;
+                PB2Counter++;   //Add to counter so after 2 seconds starts going up by 2
             }
             break;
-        case TIMER_COUNTDOWN:    //Code to handle the multiple button pushes
-            TMR3 = 0;
-            T3CONbits.TON = 1;
+        case TIMER_COUNTDOWN:    //Code to handle when timer3 causes the interrupt
+            TMR3 = 0;            //Reset the timer counter
+            T3CONbits.TON = 1;   //Re-enable the timer and start counting
             LEDOUT = !LEDOUT;   //Switch light to opposite state 
-            changeTime(-1,0);
-            if(seconds == 0 && minutes == 0){
-                state = TIMER_COMPLETED;
+            changeTime(-1,0);   //Take away a second from the timer
+            if(seconds == 0 && minutes == 0){   //Checks if the timer is done
+                state = TIMER_COMPLETED;        //Change state
             }
-            else{
-                sendMessage("CNT ");
-                state = TIMER_IDLE;
+            else{   //If not done continue countdown
+                sendMessage("CNT ");    //Send message of current timer
+                state = TIMER_IDLE;     //Go back to idle to wait for another interrupt
             }
 
             break;
-        case TIMER_PAUSED:
+        case TIMER_PAUSED:  //For when timer is paused and waiting for user input
             Idle();
             break;
-        case TIMER_COMPLETED:
-            timerActive = 0;
-            T3CONbits.TON = 0;
-            LEDOUT = 1;
-            sendMessage("FIN ");
-            timerPaused = 1;
-            Idle();
+        case TIMER_COMPLETED:   //When the timer reaches zero
+            timerActive = 0;    //Disable timer active flag
+            T3CONbits.TON = 0;  //Disable timer3
+            LEDOUT = 1;         //Set LED to on
+            sendMessage("FIN ");    //Send finish message
+            timerPaused = 0;        //reset paused flag
+            CNEN1bits.CN0IE = 1;    //Enable CN interrupt for PB1
+            CNEN1bits.CN1IE = 1;    //Enable CN interrupt for PB2
+            Idle();                 //Wait for user input
             break;
-        case TIMER_IDLE:
+        case TIMER_IDLE:    //For when timer is idle waiting for a CN or timer interrupt
             Idle();
             break;
     }
 }
     
 
-void startTimer(){
-    startTimer3();
-    state = TIMER_IDLE;
+void startTimer(){  //Starts the timer
+    //T3CON config
+    T2CONbits.T32 = 0; // operate timer 2 as 16 bit timer
+    T3CONbits.TCKPS = 1; // set prescaler to 1:8
+    T3CONbits.TCS = 0; // use internal clock
+    T3CONbits.TSIDL = 0; //operate in idle mode
+    IPC2bits.T3IP = 2; //7 is highest and 1 is lowest pri.
+    IFS0bits.T3IF = 0;
+    IEC0bits.T3IE = 1; //enable timer interrupt
+    PR3 = (31250); // set the count value for 1 s (or 1000 ms)
+    TMR3 = 0;
+    T3CONbits.TON = 1;  //Enable timer
+    timerActive = 1;    //Set flag to on
+    
+    state = TIMER_IDLE;     //Send it into idle waiting for interrupt
+    //Disable CN interrupts to prevent unwanted inputs
+    CNEN1bits.CN0IE = 0;    //Disable CN interrupt for PB1
+    CNEN1bits.CN1IE = 0;    //Disable CN interrupt for PB2
+    
 }
 void sendMessage(char* message){
     char strTime[20];
     
-    sprintf(strTime, "%02dm : %02ds", minutes, seconds);
+    sprintf(strTime, "%02dm : %02ds", minutes, seconds); //Put minutes and seconds into a string with proper fromat
     
-    char fullMessage[50];
+    char fullMessage[50];   //Array to handle the full message
     
-    strcpy(fullMessage, message);
-    strcat(fullMessage, strTime);
+    strcpy(fullMessage, message);   //Copy the beginning of the message defined in the argugment
+    strcat(fullMessage, strTime);   //Add on the minutes and seconds
     
-    if(state == TIMER_COMPLETED){
+    if(state == TIMER_COMPLETED){   //When timer is completed add on an end part
         strcat(fullMessage, " -- ALARM");
     }
             
@@ -260,50 +265,34 @@ void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void){
     //Don't forget to clear the timer 3 interrupt flag!
     IFS0bits.T3IF = 0;  //Clear flag
     T3CONbits.TON = 0;  //Disable Timer3
-    timer3Flag = 1;
-    state = TIMER_COUNTDOWN;
+    state = TIMER_COUNTDOWN;    //Set state to handle the timer3 interrupt 
 }
 
 
 void changeTime(int8_t changeSec, int8_t changeMinute){
-
-  
-    seconds += changeSec;
+    seconds += changeSec; //Change the seconds according to the argument
+    
+    //Checks if seconds overflowed if it was subtracted by 1
+    //Checks if minutes are still present
+    //Checks if you are changing the seconds
+    //This to handle when seconds reaches 0 when there is still minutes
     if(seconds == 255 && minutes && changeSec){
-        seconds = 59;
-        minutes--;
+        seconds = 59;   //Set to 59 if we reached a new minute
+        minutes--;      //Take away a minute
     }
     else{
-        minutes += changeMinute;
+        minutes += changeMinute;    //Add to mintues
 
     }
     
+    //If seconds overflow 60
     if(seconds >= 60){
-        seconds = seconds - 60;        
-        minutes++;
+        seconds = seconds % 60;   //Find remainder of seconds     
+        minutes++;                //Add to minutes
     }
 
     if(minutes >= 60){
-        minutes = 0;
+        minutes = 0;    //Set minutes to 0 is overflowed over 60
     } 
-    
-    
-
-    
-}
-
-void startTimer3(){
-    //T3CON config
-    T2CONbits.T32 = 0; // operate timer 2 as 16 bit timer
-    T3CONbits.TCKPS = 1; // set prescaler to 1:8
-    T3CONbits.TCS = 0; // use internal clock
-    T3CONbits.TSIDL = 0; //operate in idle mode
-    IPC2bits.T3IP = 2; //7 is highest and 1 is lowest pri.
-    IFS0bits.T3IF = 0;
-    IEC0bits.T3IE = 1; //enable timer interrupt
-    PR3 = (15625*2); // set the count value for 1 s (or 1000 ms)
-    TMR3 = 0;
-    T3CONbits.TON = 1;
-    timerActive = 1;
 }
 
--- project1.X/nbproject/Makefile-default.mk
+++ project1.X/nbproject/Makefile-default.mk
@@ -89,62 +89,62 @@ MP_LINKER_FILE_OPTION=,--script=p24F16KA101.gld
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/d80962e73f27d693afd6904e28514927ccf17cf0 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/4237ab07b7f944625574f781b1abc25d8eb3e26 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/IOs.o: IOs.c  .generated_files/flags/default/3f943bae1ff086b2d3b024b9f5b496b9083edc4c .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/IOs.o: IOs.c  .generated_files/flags/default/30e5ce6866a5d9c0ef8d1a7621d272c56bc8aec3 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/IOs.o.d 
 	@${RM} ${OBJECTDIR}/IOs.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  IOs.c  -o ${OBJECTDIR}/IOs.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/IOs.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/clkChange.o: clkChange.c  .generated_files/flags/default/108b6b5ca8a1925f5d5bb6f4e9476cbfa54cc2e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/clkChange.o: clkChange.c  .generated_files/flags/default/4e7026f1c8ecd5cf2132736caa7657860b06ddf .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/clkChange.o.d 
 	@${RM} ${OBJECTDIR}/clkChange.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  clkChange.c  -o ${OBJECTDIR}/clkChange.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/clkChange.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/TimeDelay.o: TimeDelay.c  .generated_files/flags/default/4fa4731876f5e1b2cc5173f20281e631d984d24e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/TimeDelay.o: TimeDelay.c  .generated_files/flags/default/8d44c6c0d13a11ee440ee5c263bd5e41596734eb .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/TimeDelay.o.d 
 	@${RM} ${OBJECTDIR}/TimeDelay.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  TimeDelay.c  -o ${OBJECTDIR}/TimeDelay.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/TimeDelay.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/UART.o: UART.c  .generated_files/flags/default/c4c622df3438ccb09b610a2fcd7011d08498925 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/UART.o: UART.c  .generated_files/flags/default/675664571bd5fe111e845b920cd3c61f4c74131 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART.o.d 
 	@${RM} ${OBJECTDIR}/UART.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  UART.c  -o ${OBJECTDIR}/UART.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/UART.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 else
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/f9ff3561b05f8d87989320ab629033d7dd2d684b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/c80453ff8e11b4a3a1d4c2840f610622483a55ff .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/IOs.o: IOs.c  .generated_files/flags/default/fde4475fdab7aee68561d38284be98ad9a5a2206 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/IOs.o: IOs.c  .generated_files/flags/default/5cbe6dca306be42446ae3eafcc61e61cc8f40617 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/IOs.o.d 
 	@${RM} ${OBJECTDIR}/IOs.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  IOs.c  -o ${OBJECTDIR}/IOs.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/IOs.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/clkChange.o: clkChange.c  .generated_files/flags/default/7c1d592456282961b7355d60b6b8fa3416a31191 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/clkChange.o: clkChange.c  .generated_files/flags/default/168922715f5fee264aea65289d8d8df3aef17bdc .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/clkChange.o.d 
 	@${RM} ${OBJECTDIR}/clkChange.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  clkChange.c  -o ${OBJECTDIR}/clkChange.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/clkChange.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/TimeDelay.o: TimeDelay.c  .generated_files/flags/default/db965cde0094e88be1371f2bbc888c8d43fbcc63 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/TimeDelay.o: TimeDelay.c  .generated_files/flags/default/7f7edb28bc5aef03f9160a072de34f5f01c57edc .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/TimeDelay.o.d 
 	@${RM} ${OBJECTDIR}/TimeDelay.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  TimeDelay.c  -o ${OBJECTDIR}/TimeDelay.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/TimeDelay.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/UART.o: UART.c  .generated_files/flags/default/27a1efeca57eb64291f533e086e5da87edfa534d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/UART.o: UART.c  .generated_files/flags/default/6df7d18e5d29d3c44472eaa22c7a9d1bd3ce3360 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART.o.d 
 	@${RM} ${OBJECTDIR}/UART.o 
