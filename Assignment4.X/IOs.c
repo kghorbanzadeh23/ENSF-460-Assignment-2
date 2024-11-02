d1b99e13a4f7ec24d5c2d595bbff7145a586d636
HuttonLedingham <huttonledingham@gmail.com>
2024-11-02 16:59:53 -0600

    comments added

--- Assignment4.X/IOs.c
+++ Assignment4.X/IOs.c
@@ -46,18 +46,18 @@ void StateInit(){
 }
 
 void sendMessage(char* message){
-    char clear[61] = "\033[2J \033[H ";
-    strcat(clear, message);
-    Disp2String(clear);
+    char clear[61] = "\033[2J \033[H "; //Clears terminal
+    strcat(clear, message);             //Add on message after clear
+    Disp2String(clear);                 //Display whole message
 }
 
 void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void){
-    if(state == MODEZERO && !PB1){
-        state = MODEONE;
+    if(state == MODEZERO && !PB1){  //Check what state and PB1 is pressed
+        state = MODEONE;            //Change the state to ModeOne if ModeZero and PB1
     }
-    else if (state == MODEONE && !PB1){
-        state = MODEZERO;
-        previousADCvalue = 1;
+    else if (state == MODEONE && !PB1){ //Check what state and PB1 is pressed
+        state = MODEZERO;           //Change the state to ModeOne if ModeZero and PB1
+        previousADCvalue = 1;       //Change to 1 so the screen will update
     }
     IFS1bits.CNIF = 0;     //Clear the CN interrupt flag
 }
@@ -65,28 +65,28 @@ void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void){
 void IOcheck(){
     switch(state){
         case MODEZERO:;
-            char line[45] = "Mode 0: ";
-            LEDOUT = 0;
-            ADCvalue = do_ADC();
-            uint8_t counter = ADCvalue * 0.031 + 1;
-            for(int i = 0; i < counter; i++){
-                strcat(line, "*");
+            char line[45] = "Mode 0: "; //Starting of message
+            LEDOUT = 0;                 //Turn LED off to help identify what mode it is in
+            ADCvalue = do_ADC();        //Grab ADC value from pin 9
+            uint8_t counter = ADCvalue * 0.031 + 1; //Calculate how many asterisks there are
+            for(int i = 0; i < counter; i++){   //Add asterisks to message according the counter value is
+                strcat(line, "*");  //Add asterisk to message
             }
-            if(ADCvalue != previousADCvalue){
-                previousADCvalue = ADCvalue;
-                sendMessage(line);
-                Disp2Hex(ADCvalue);
+            if(ADCvalue != previousADCvalue){   //If the value has changed since last time we check update the value
+                previousADCvalue = ADCvalue;    //Set previous to current
+                sendMessage(line);              //Send message over UART
+                Disp2Hex(ADCvalue);             //Send ADC value in hex format
             }
-            delay_ms(1000);
+            delay_ms(1000);                     //Delay for a second
 
             break;
         case MODEONE:
-            LEDOUT = 1;
-            char stringADC[10];
-            ADCvalue = do_ADC();
+            LEDOUT = 1;     //Turn LED on to help debug what state
+            char stringADC[10];     //Initialize array for UART message
+            ADCvalue = do_ADC();    //Grab ADC value
 
-            sprintf(stringADC, "%d \n", ADCvalue);
-            Disp2String(stringADC);
+            sprintf(stringADC, "%d \n", ADCvalue);  //Put ADC value in a string with correct formatting
+            Disp2String(stringADC);                 //Send string over UART
             break;
     }
 }
